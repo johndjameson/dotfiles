3400d13b7876aa9a84681493b5d8e4532f60b6e7
John D. Jameson <thejohnjameson@gmail.com>
2025-07-25 21:16:15 -0500

    Update nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,9 @@
 vim.g.mapleader = " "
 
-vim.opt.number = true
+vim.opt.number = true -- Show line numbers (absolute)
+
+vim.opt.hlsearch = false -- Disable sticky search results
+vim.opt.ignorecase = true -- Ignore case when searching
+vim.opt.smartcase = true -- Match case when searching if using any uppercase
 
 vim.api.nvim_set_keymap('n', '<leader>w', ':w<CR>', { desc = 'Write file', noremap = true })
