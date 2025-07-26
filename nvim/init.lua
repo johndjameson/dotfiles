d958323ae911c4bfb6ec7b2df2c076681219b3ba
John D. Jameson <thejohnjameson@gmail.com>
2025-07-25 21:51:26 -0500

    Set up lazy.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,6 @@
 vim.g.mapleader = " "
 
+vim.opt.termguicolors = true
 vim.opt.number = true -- Show line numbers (absolute)
 
 vim.opt.hlsearch = false -- Disable sticky search results
@@ -7,3 +8,5 @@ vim.opt.ignorecase = true -- Ignore case when searching
 vim.opt.smartcase = true -- Match case when searching if using any uppercase
 
 vim.api.nvim_set_keymap('n', '<leader>w', ':w<CR>', { desc = 'Write file', noremap = true })
+
+require("config.lazy")
