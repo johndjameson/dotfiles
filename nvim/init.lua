be65cf1054362d0ff9d67d7526977ce4cc6e313a
John D. Jameson <thejohnjameson@gmail.com>
2025-07-25 22:05:39 -0500

    Update nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,6 @@
-vim.g.mapleader = " "
+vim.g.mapleader = " " -- Map leader to spacebar
 
-vim.opt.termguicolors = true
+vim.opt.termguicolors = true -- Enable HEX colors
 vim.opt.number = true -- Show line numbers (absolute)
 
 vim.opt.hlsearch = false -- Disable sticky search results
@@ -8,5 +8,6 @@ vim.opt.ignorecase = true -- Ignore case when searching
 vim.opt.smartcase = true -- Match case when searching if using any uppercase
 
 vim.api.nvim_set_keymap('n', '<leader>w', ':w<CR>', { desc = 'Write file', noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>x', ':x<CR>', { desc = 'Write file and close', noremap = true })
 
 require("config.lazy")
