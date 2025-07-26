e1a313e12361ca4f70ff0d888a7a1c9a31724ca0
John D. Jameson <thejohnjameson@gmail.com>
2025-07-26 09:19:09 -0500

    Update nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,9 @@
-vim.g.mapleader = " " -- Map leader to spacebar
+vim.g.mapleader = ' ' -- Map leader to spacebar
 
 vim.opt.termguicolors = true -- Enable HEX colors
 vim.opt.number = true -- Show line numbers (absolute)
+
+vim.opt.clipboard = 'unnamedplus' -- Copy and paste using system clipboard
 
 vim.opt.hlsearch = false -- Disable sticky search results
 vim.opt.ignorecase = true -- Ignore case when searching
@@ -9,5 +11,7 @@ vim.opt.smartcase = true -- Match case when searching if using any uppercase
 
 vim.api.nvim_set_keymap('n', '<leader>w', ':w<CR>', { desc = 'Write file', noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>x', ':x<CR>', { desc = 'Write file and close', noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>q', ':bd!<CR>', { desc = 'Close buffer', noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>Q', ':qa!<CR>', { desc = 'Close all', noremap = true })
 
 require("config.lazy")
