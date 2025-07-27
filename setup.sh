b29df25bafe52894cd6c810950e0ec2e14dd4b94
John D. Jameson <thejohnjameson@gmail.com>
2025-07-27 11:31:15 -0500

    Remove gitconfig from setup

--- setup.sh
+++ setup.sh
@@ -12,7 +12,6 @@ ln -fns ~/.dotfiles/ag/agignore ~/.agignore
 ln -fns ~/.dotfiles/bash/bash_profile ~/.bash_profile
 ln -fns ~/.dotfiles/bash/hushlogin ~/.hushlogin
 ln -fns ~/.dotfiles/bash/inputrc ~/.inputrc
-ln -fns ~/.dotfiles/git/gitconfig ~/.gitconfig
 ln -fns ~/.dotfiles/nvim ~/.config/nvim
 ln -fns ~/.dotfiles/starship/starship.toml ~/.config/starship.toml
 ln -fns ~/.dotfiles/vim ~/.vim
