7079b60112323305ac2234080517c10c286e66df
John D. Jameson <thejohnjameson@gmail.com>
2025-07-28 18:52:19 -0500

    Add redo mapping

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,9 +9,10 @@ vim.opt.hlsearch = false -- Disable sticky search results
 vim.opt.ignorecase = true -- Ignore case when searching
 vim.opt.smartcase = true -- Match case when searching if using any uppercase
 
+vim.api.nvim_set_keymap('n', '<leader>Q', ':qa!<CR>', { desc = 'Close all', noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>q', ':bd!<CR>', { desc = 'Close buffer', noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>w', ':w<CR>', { desc = 'Write file', noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>x', ':x<CR>', { desc = 'Write file and close', noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>q', ':bd!<CR>', { desc = 'Close buffer', noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>Q', ':qa!<CR>', { desc = 'Close all', noremap = true })
+vim.api.nvim_set_keymap('n', 'U', ':redo<CR>', { desc = 'Redo', noremap = true })
 
 require("config.lazy")
