166b8781369c36c7f47daef3e885519c0b449664
John D. Jameson <thejohnjameson@gmail.com>
2025-07-29 21:00:13 -0500

    Add telescope config

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -27,7 +27,29 @@ return {
 	'nvim-telescope/telescope.nvim', branch = '0.1.x',
 		dependencies = { 'nvim-lua/plenary.nvim' },
 		config = function()
-			require('telescope').setup({})
+			require('telescope').setup({
+				defaults = {
+					layout_strategy = 'vertical',
+					layout_config = {
+						vertical = {
+							height = 0.6,
+							mirror = true,
+							prompt_position = 'top',
+						},
+					},
+					mappings = {
+						i = {
+							['<C-j>'] = require('telescope.actions').move_selection_next,
+							['<C-k>'] = require('telescope.actions').move_selection_previous,
+						},
+						n = {
+							['<C-j>'] = require('telescope.actions').move_selection_next,
+							['<C-k>'] = require('telescope.actions').move_selection_previous,
+						},
+					},
+					sorting_strategy = 'ascending',
+				},
+			})
 			local builtin = require('telescope.builtin')
 			vim.keymap.set('n', '<c-f>', builtin.find_files, { desc = 'Telescope find files' })
 			vim.keymap.set('n', '<c-g>', builtin.live_grep, { desc = 'Telescope live grep' })
