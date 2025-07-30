40904d1469ffbb891514fbed7a0cce7c9f0a8b6e
John D. Jameson <thejohnjameson@gmail.com>
2025-07-29 19:14:44 -0500

    Set lazy install colorscheme

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -28,6 +28,7 @@ require("lazy").setup({
     -- import your plugins
     { import = "plugins" },
   },
+	install = { colorscheme = { "solarized" } },
   -- automatically check for plugin updates
   checker = {
 		enabled = true,
