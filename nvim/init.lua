0670338728abf3cae0229f223c65169ccf1a7733
John D. Jameson <thejohnjameson@gmail.com>
2025-07-30 04:48:04 -0500

    Update nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,6 +5,7 @@ vim.opt.listchars = { tab = '» ', trail = '·' }
 vim.opt.termguicolors = true -- Enable HEX colors
 vim.opt.number = true -- Show line numbers (absolute)
 vim.opt.signcolumn = 'yes'
+vim.opt.wrap = false
 
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
@@ -15,12 +16,16 @@ vim.opt.hlsearch = false -- Disable sticky search results
 vim.opt.ignorecase = true -- Ignore case when searching
 vim.opt.smartcase = true -- Match case when searching if using any uppercase
 
-vim.api.nvim_set_keymap('n', '<leader>Q', ':qa!<CR>', { desc = 'Close all', noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>q', ':bd!<CR>', { desc = 'Close buffer', noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>w', ':w<CR>', { desc = 'Write file', noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>x', ':x<CR>', { desc = 'Write file and close', noremap = true })
-vim.api.nvim_set_keymap('n', 'U', ':redo<CR>', { desc = 'Redo', noremap = true })
-vim.api.nvim_set_keymap('n', 'H', ':bprev<CR>', { desc = 'Next buffer', noremap = true })
-vim.api.nvim_set_keymap('n', 'L', ':bnext<CR>', { desc = 'Previous buffer', noremap = true })
+vim.keymap.set('n', '<c-h>', '<c-w><c-h>', { desc = 'Left panel' })
+vim.keymap.set('n', '<c-j>', '<c-w><c-j>', { desc = 'Down panel' })
+vim.keymap.set('n', '<c-k>', '<c-w><c-k>', { desc = 'Up panel' })
+vim.keymap.set('n', '<c-l>', '<c-w><c-l>', { desc = 'Right panel' })
+vim.keymap.set('n', '<leader>Q', ':qa!<CR>', { desc = 'Close all' })
+vim.keymap.set('n', '<leader>q', ':bd!<CR>', { desc = 'Close buffer' })
+vim.keymap.set('n', '<leader>w', ':w<CR>', { desc = 'Write file' })
+vim.keymap.set('n', '<leader>x', ':x<CR>', { desc = 'Write file and close' })
+vim.keymap.set('n', 'H', ':bprev<CR>', { desc = 'Next buffer', silent = true })
+vim.keymap.set('n', 'L', ':bnext<CR>', { desc = 'Previous buffer', silent = true })
+vim.keymap.set('n', 'U', ':redo<CR>', { desc = 'Redo' })
 
 require("config.lazy")
