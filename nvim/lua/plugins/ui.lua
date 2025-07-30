27de0cbbe4d7c6d1bc593c89c6adb1ff5ff3e996
John D. Jameson <thejohnjameson@gmail.com>
2025-07-29 20:04:09 -0500

    Add nvim telescope

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -23,4 +23,12 @@ return {
 			})
 		end,
 	},
+	{
+	'nvim-telescope/telescope.nvim', branch = '0.1.x',
+		dependencies = { 'nvim-lua/plenary.nvim' }
+	},
+	config = function ()
+		local builtin = require('telescope.builtin')
+		vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+	end
 }
