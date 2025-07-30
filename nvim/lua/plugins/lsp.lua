bd45d15b532e36a51fa48fe1bb8a9c269da97bc4
John D. Jameson <thejohnjameson@gmail.com>
2025-07-30 17:47:31 -0500

    Set up treesitter highlighting

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,8 +1,40 @@
 return {
-    "mason-org/mason-lspconfig.nvim",
-    opts = {},
-    dependencies = {
-        { "mason-org/mason.nvim", opts = {} },
-        "neovim/nvim-lspconfig",
-    },
+	{
+		"mason-org/mason-lspconfig.nvim",
+		opts = {},
+		dependencies = {
+				{ "mason-org/mason.nvim", opts = {} },
+				"neovim/nvim-lspconfig",
+		},
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		branch = 'master',
+		lazy = false,
+		build = ":TSUpdate",
+		config = function ()
+			require'nvim-treesitter.configs'.setup({
+				ensure_installed = {
+					"bash",
+					"css",
+					"gitcommit",
+					"gitignore",
+					"html",
+					"javascript",
+					"json",
+					"json5",
+					"lua",
+					"markdown",
+					"markdown_inline",
+					"python",
+					"rust",
+					"toml",
+					"tsx",
+					"typescript",
+					"vim",
+				},
+				highlight = { enable = true }
+			})
+		end
+	}
 }
